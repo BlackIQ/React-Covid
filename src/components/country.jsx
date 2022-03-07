3d6647465c4f5aaf22afb194e84f12183a96e018
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-07 14:12:38 +0330

    Fix props and add render country

---
+++ src/components/country.jsx
@@ -0,0 +1,30 @@
+import {FaHospital, FaPlus, FaSkull} from "react-icons/fa";
+
+const Country = (country) => {
+    console.log(country.country);
+
+    return (
+        <div className="col-md-3">
+            <div className="m-3 card border">
+                <h4 className="card-header">{ country.country.country_name }</h4>
+                <div className="card-body">
+                    <span>
+                        <span className="text-primary">
+                            <FaHospital /> <b>Total Cases</b> { country.country.total_cases }
+                        </span>
+                        <br />
+                        <span className="text-success">
+                            <FaPlus /> <b>Recovered</b> { country.country.total_recovered }
+                        </span>
+                        <br />
+                        <span className="text-danger">
+                            <FaSkull /> <b>Died</b> { country.country.total_deaths }
+                        </span>
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Country;
