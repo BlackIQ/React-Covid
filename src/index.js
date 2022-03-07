dba9ba71fef9bcf4de05480cd265388cce3eaab8
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-07 14:13:07 +0330

    Import material design

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'mdb-ui-kit/css/mdb.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
