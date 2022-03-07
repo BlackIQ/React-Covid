0a918b315eb0f769feb68a3af3d8651f32dabb9a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-07 15:05:26 +0330

    Move loading in a single component

---
+++ src/components/loading.jsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+    return (
+        <div className="m-3">
+            <hr />
+            <h4>Loading data . . .</h4>
+        </div>
+    );
+}
+
+export default Loading;
